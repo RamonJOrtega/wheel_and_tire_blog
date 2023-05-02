581c4823f7764ced86f36091d69a5f4b83e5add3
ramon6767 <ramon.justis.ortega@gmail.com>
2023-05-01 17:58:35 -0700

    add header and footer elements to layout.tsx component

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,10 +13,29 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
+  const header = 
+    (<header>
+      <div>
+        <h1>Wheel and Tire Blog</h1>
+        <p>Welcome to you're dedicated wheel and tire discussions</p>
+      </div>
+    </header>)
+
+  const footer = (
+    <footer>
+      <div>
+        <br></br>
+        <h3>Developed by Calculated Customs</h3>
+      </div>
+    </footer>
+  )
   return (
     <html lang="en">
       <head />
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {header}
+        {children}</body>
+        {footer}
     </html>
   )
 }
