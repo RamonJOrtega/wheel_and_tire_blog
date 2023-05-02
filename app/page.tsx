d0244179e4ec08043fb42a9ee80e1c632320089a
ramon6767 <ramon.justis.ortega@gmail.com>
2023-05-02 15:10:18 -0700

    add fs to get slugs and postMetadata in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,27 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import fs from "fs"
 
 const inter = Inter({ subsets: ['latin'] })
 
+const getPostMetadata = () => {
+  const path = require('path')
+  const dirPath = path.resolve(__dirname, './posts')
+  const files = fs.readdirSync(dirPath)
+  const markdownPosts = files.filter((file) => file.endsWith(".md"))
+  const slugs = markdownPosts.map((file) => file.replace(".md", ""))
+  return slugs
+}
+
 export default function Home() {
+  const postMetaData = getPostMetadata()
+  const postPreviews = postMetaData.map((slug) => (
+    <div>
+      <h2>{slug}</h2>
+    </div>
+  ))
   return (
-    <h1>Hello from NextJS 13!</h1>
+
+    <div>{postPreviews}</div>
   )
 }
