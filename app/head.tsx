26e6d4bcb663c166dc3833d3279980640eafc36d
ramon6767 <ramon.justis.ortega@gmail.com>
2023-05-01 17:53:55 -0700

    add title to page

--- app/head.tsx
+++ app/head.tsx
@@ -1,7 +1,7 @@
 export default function Head() {
     return (
         <>
-            <title></title>
+            <title>Wheel and Tire Blog</title>
             <meta content = "width=device-width, initial-scale=1">  </meta>
             <link rel="icon" href="/favicon.ico"></link>
         </>
